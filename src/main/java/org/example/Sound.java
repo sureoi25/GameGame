2cf48ddec547cd3185445ab44d6679785e663236
unknown <juddplug@gmail.com>
2025-03-23 22:06:17 +0800

    unfinished music things

---
+++ src/main/java/org/example/Sound.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.sound.sampled.Clip;
+import java.net.URL;
+
+public class Sound {
+    Clip clip;
+    URL sound_url[] = new URL[70];
+
+    public Sound(){
+
+        sound_url[0] = getClass().getResource("/Sound/Cynthia boss battle theme.mp3");
+        sound_url[1] = getClass().getResource("/Sound/Death sound.mp3");
+        sound_url[2] = getClass().getResource("/Sound/hurt.wav");
+        sound_url[3] = getClass().getResource("/Sound/little root town theme.mp3");
+        sound_url[4] = getClass().getResource("/Sound/Minecraft Eating.mp3");
+        sound_url[5] = getClass().getResource("/Sound/Open chest.wav");
+        sound_url[6] = getClass().getResource("/Sound/power_up.wav");
+
+    }
+}
